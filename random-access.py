c684d83c5dea64324283533c140cb965bf8ecc76
Keith Henderson Jr <keith@debian>
2013-02-23 20:22:26 -0500

    Added random-access.py

---
+++ random-access.py
@@ -0,0 +1,21 @@
+# Demonstrates string indexing
+
+import random
+
+word = input("Enter a word: ")
+print("The word is: ", word, "\n")
+
+high = len(word)
+low = -len(word)
+
+for i in range(10):
+	position = random.randrange(low, high)
+	print("word[", position, "]\t", word[position])
+
+# Printing a string backwards using string indexing
+word = input("Enter a word: ")
+print(word[-1])
+print(word[-2])
+print(word[-3])
+print(word[-4])
+print(word[-5])
