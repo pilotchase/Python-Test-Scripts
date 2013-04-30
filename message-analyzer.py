755f20d842fe7140fb7fb2d5f3c557ddab439643
Keith Henderson Jr <keith@debian>
2013-04-30 15:41:04 -0400

    message-analyzer.py now recognizes both upper and lower case characters

--- message-analyzer.py
+++ message-analyzer.py
@@ -1,21 +1,14 @@
 # Demonstrates the "len()" function and the "in" operator
 
-vowels = "a", "e", "i", "o", "u", "y",
+vowels = "a", "e", "i", "o", "u", "y"
 message = input("Enter a message: ")
 
 print("\nThe length of your message is", len(message), "characters")
 
 print("\nYour message contains the following vowels:")
-
-if "a" in message:
-	print("a")
-if "e" in message:
-	print("e")
-if "i" in message:
-	print("i")
-if "o" in message:
-	print("o")
-if "u" in message:
-	print("u")
-if "y" in message:
-	print("y")
+for letter in message:
+	vowels == message.upper()
+	if letter.lower() in vowels:
+		print(letter.lower())
+	if letter.lower() not in vowels:
+		print("None")
---
+++ pizza-slicer.py
@@ -0,0 +1,16 @@
+# Pizza Slicer
+# Demonstrates string slicing
+
+word = "pizza"
+
+print(
+"""
+Slicing 'Cheat Sheet'
+
+ 0   1   2   3   4   5
+ +---+---+---+---+---+
+ | p | i | z | z | a |
+-5  -4  -3  -2  -1
+
+"""
+)
